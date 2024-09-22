494af5b24f369c44ab3cae5af68b6b6e38b2cf98
Angel Usiel Ruiz Regino <130244709+usi311@users.noreply.github.com>
2024-09-21 21:42:42 -0600

    Revert "add skills branch closes #12 (#8)"
    This reverts commit f0d40e8a8ee343264f539b10693655c77c9497af.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent, SkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
